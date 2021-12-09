5f57be0a7ce01f0eaee80349593b72f250129b82
Eoic <karolis.strazdas@protonmail.com>
2021-12-09 20:18:05 +0200

    Add day 9, part 2.

--- Day 9/main.ex
+++ Day 9/main.ex
@@ -1,5 +1,5 @@
 defmodule DayNine do
-  @input_path "input"
+  @input_path "large"
 
   def get_input do
     {:ok, data} = File.read(@input_path)
@@ -11,16 +11,82 @@ defmodule DayNine do
   end
 
   def part_one(data) do
+    get_lowest_points(data) |> Enum.reduce(0, fn %{element: value}, sum -> sum + value + 1 end)
+  end
+
+  def part_two(data) do
+    row_length = data |> Enum.count()
+    col_length = data |> Enum.at(0) |> Enum.count()
+    visited_positions_map = create_visited_positions_map(data)
+
+    get_lowest_points(data)
+    |> Enum.map(fn %{position: lowest_position} ->
+      fill([row_length, col_length], data, visited_positions_map, [lowest_position])
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.product()
+  end
+
+  def create_visited_positions_map(data) do
+    data
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {row, row_index}, positions_map_outer ->
+      row
+      |> Enum.with_index()
+      |> Enum.reduce(positions_map_outer, fn {element, column_index}, positions_map_inner ->
+        is_visited = if element === 9, do: true, else: false
+        Map.put(positions_map_inner, [row_index, column_index], is_visited)
+      end)
+    end)
+  end
+
+  def get_adjacent_positions(row_index, column_index) do
+    [
+      [row_index, column_index - 1],
+      [row_index, column_index + 1],
+      [row_index + 1, column_index],
+      [row_index - 1, column_index]
+    ]
+  end
+
+  def fill(size, data, visited_positions, queued_positions, basin_size \\ 0)
+
+  def fill(_, _, _, [], basin_size), do: basin_size
+
+  def fill(size, data, visited_positions, [next_position | queued_positions], basin_size) do
+    [row_length, column_length] = size
+    [row_index, column_index] = next_position
+
+    next_positions =
+      get_adjacent_positions(row_index, column_index)
+      |> Enum.filter(fn [row, column] ->
+        is_visited = Map.get(visited_positions, [row, column])
+        is_queued = Enum.member?(queued_positions, [row, column])
+        is_length_valid = row >= 0 && column >= 0 && column < column_length && row < row_length
+        !is_queued && !is_visited && is_length_valid
+      end)
+
+    fill(
+      size,
+      data,
+      Map.put(visited_positions, [row_index, column_index], true),
+      next_positions ++ queued_positions,
+      basin_size + 1
+    )
+  end
+
+  def get_lowest_points(data) do
     row_length = data |> Enum.count()
     col_length = data |> Enum.at(0) |> Enum.count()
 
     data
     |> Enum.with_index()
-    |> Enum.reduce(0, fn {row, row_index}, sum_acc ->
-      sum =
+    |> Enum.reduce([], fn {row, row_index}, lowest_elements ->
+      lowest_row_elements =
         row
         |> Enum.with_index()
-        |> Enum.reduce([], fn {col, col_index}, lowest_elements_acc ->
+        |> Enum.reduce([], fn {col, col_index}, lowest_elements_per_column ->
           current_element = col
 
           is_lowest =
@@ -31,7 +97,9 @@ defmodule DayNine do
               [row_index - 1, col_index]
             ]
             |> Enum.filter(fn [w_row_index, w_col_index] ->
-              w_row_index >= 0 && w_col_index >= 0 && w_col_index < col_length &&
+              w_row_index >= 0 &&
+                w_col_index >= 0 &&
+                w_col_index < col_length &&
                 w_row_index < row_length
             end)
             |> Enum.all?(fn [w_row_index, w_col_index] ->
@@ -40,19 +108,20 @@ defmodule DayNine do
             end)
 
           if is_lowest do
-            [current_element + 1 | lowest_elements_acc]
+            [
+              %{
+                :element => current_element,
+                :position => [row_index, col_index]
+              }
+              | lowest_elements_per_column
+            ]
           else
-            lowest_elements_acc
+            lowest_elements_per_column
           end
         end)
-        |> Enum.sum()
 
-      sum_acc + sum
+      lowest_row_elements ++ lowest_elements
     end)
-  end
-
-  def part_two(data) do
-    nil
   end
 
   def run do
