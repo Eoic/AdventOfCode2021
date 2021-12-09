c76969a4cb0b4bd70966536f6bcd1631a696ed52
Eoic <karolis.strazdas@protonmail.com>
2021-12-09 04:48:40 +0200

    Refactoring.

--- Day 8/main.ex
+++ Day 8/main.ex
@@ -1,5 +1,29 @@
 defmodule DayEight do
-  @input_path "input_large"
+  @input_path "input"
+
+  @permutations [
+    [1, 0, 0, 1, 1, 0],
+    [1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 0, 1],
+    [0, 1, 0, 1, 1, 0],
+    [0, 1, 0, 1, 0, 1],
+    [0, 1, 1, 0, 1, 0],
+    [0, 1, 1, 0, 0, 1]
+  ]
+
+  @segments_map %{
+    0 => [0, 1, 3, 4, 5, 6],
+    1 => [5, 6],
+    2 => [0, 2, 3, 4, 6],
+    3 => [0, 2, 4, 5, 6],
+    4 => [1, 2, 5, 6],
+    5 => [0, 1, 2, 4, 5],
+    6 => [0, 1, 2, 3, 4, 5],
+    7 => [0, 5, 6],
+    8 => [0, 1, 2, 3, 4, 5, 6],
+    9 => [0, 1, 2, 4, 5, 6]
+  }
 
   def get_input do
     {:ok, data} = File.read(@input_path)
@@ -10,20 +34,14 @@ defmodule DayEight do
     |> Enum.map(&String.split(&1, " | "))
   end
 
-  def contains_length(element) do
-    Enum.member?([2, 3, 4, 7], element)
-  end
-
   def part_one(data) do
     total_count =
       data
-      |> Enum.reduce(0, fn [input, output], count_acc ->
+      |> Enum.reduce(0, fn [_, output], count_acc ->
         count =
           output
           |> String.split(" ")
-          |> Enum.filter(fn element ->
-            element |> String.graphemes() |> Enum.count() |> contains_length()
-          end)
+          |> Enum.filter(&(String.graphemes(&1) |> Enum.count() |> contains_length()))
           |> Enum.count()
 
         count_acc + count
@@ -32,7 +50,49 @@ defmodule DayEight do
     total_count
   end
 
-  def select_known_segments(input) do
+  def part_two(data) do
+    segments_map_inverse = Enum.into(@segments_map, %{}, &{elem(&1, 1), elem(&1, 0)})
+
+    data
+    |> Enum.map(fn [input, output] ->
+      input_split = String.split(input, " ")
+      output_split = String.split(output, " ")
+      segments = create_segments_map(input_split)
+
+      tbr = segments
+      |> Map.get(1)
+      |> Enum.to_list()
+
+      tlm = MapSet.difference(
+        Map.get(segments, 4),
+        Map.get(segments, 1)
+      ) |> Enum.to_list()
+
+      top = MapSet.difference(
+        Map.get(segments, 7),
+        Map.get(segments, 1)
+      ) |> Enum.to_list()
+
+      bbl = MapSet.union(
+        Map.get(segments, 4),
+        Map.get(segments, 7))
+        |> (&MapSet.difference(Map.get(segments, 8), &1)).()
+        |> Enum.to_list()
+
+      create_display_configurations(@permutations, %{0 => top, 1 => tlm, 2 => tlm, 3 => bbl, 4 => bbl, 5 => tbr, 6 => tbr}, @segments_map, [])
+      |> Enum.find(&validate_input(&1, @segments_map, input_split))
+      |> decode_output_display(segments_map_inverse, output_split, [])
+      |> Enum.join()
+      |> String.to_integer()
+    end)
+    |> Enum.sum()
+  end
+
+  def contains_length(element) do
+    Enum.member?([2, 3, 4, 7], element)
+  end
+
+  def create_segments_map(input) do
     input
     |> Enum.reduce(%{}, fn element, segment_map ->
       case element |> String.graphemes() |> Enum.count() do
@@ -45,90 +105,22 @@ defmodule DayEight do
     end)
   end
 
-  def part_two(data) do
-    permutations = [
-      [1, 0, 0, 1, 1, 0],
-      [1, 0, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0],
-      [1, 0, 1, 0, 0, 1],
-      [0, 1, 0, 1, 1, 0],
-      [0, 1, 0, 1, 0, 1],
-      [0, 1, 1, 0, 1, 0],
-      [0, 1, 1, 0, 0, 1]
-    ]
-
-    segments_map = %{
-      0 => [0, 1, 3, 4, 5, 6],
-      1 => [5, 6],
-      2 => [0, 2, 3, 4, 6],
-      3 => [0, 2, 4, 5, 6],
-      4 => [1, 2, 5, 6],
-      5 => [0, 1, 2, 4, 5],
-      6 => [0, 1, 2, 3, 4, 5],
-      7 => [0, 5, 6],
-      8 => [0, 1, 2, 3, 4, 5, 6],
-      9 => [0, 1, 2, 4, 5, 6]
-    }
-
-    segments_map_inv = Enum.into(segments_map, %{}, &{elem(&1, 1), elem(&1, 0)})
-
-    data
-    |> Enum.map(fn [input, output] ->
-      input_split = String.split(input, " ")
-      output_split = String.split(output, " ")
-      segments = select_known_segments(input_split)
-      topAndBottomRight = Map.get(segments, 1) |> Enum.to_list()
-
-      topLeftMiddle =
-        MapSet.difference(Map.get(segments, 4), Map.get(segments, 1)) |> Enum.to_list()
-
-      top = MapSet.difference(Map.get(segments, 7), Map.get(segments, 1)) |> Enum.to_list()
-
-      bottomBottomLeft =
-        MapSet.union(Map.get(segments, 4), Map.get(segments, 7))
-        |> (&MapSet.difference(Map.get(segments, 8), &1)).()
-        |> Enum.to_list()
-
-      segment_reference = %{
-        0 => top,
-        1 => topLeftMiddle,
-        2 => topLeftMiddle,
-        3 => bottomBottomLeft,
-        4 => bottomBottomLeft,
-        5 => topAndBottomRight,
-        6 => topAndBottomRight
-      }
-
-      configs = get_valid_display_config(permutations, segment_reference, segments_map, [])
-
-      valid_config =
-        Enum.find(configs, fn config ->
-          validate_input(config, segments_map, input_split, false)
-        end)
-
-      decode_output_display(valid_config, segments_map_inv, output_split, [])
-      |> Enum.join()
-      |> String.to_integer()
-    end)
-    |> Enum.sum()
-  end
-
   def decode_output_display(_, _, [], output), do: Enum.reverse(output)
 
-  def decode_output_display(config, segments_map_inv, [output_segment | queue], output) do
+  def decode_output_display(config, segments_map_inverse, [output_segment | queue], output) do
     positions =
       output_segment
       |> String.graphemes()
       |> Enum.map(&Map.get(config, &1))
       |> Enum.sort()
 
-    matching_number = Map.get(segments_map_inv, positions)
-    decode_output_display(config, segments_map_inv, queue, [matching_number | output])
+    matching_number = Map.get(segments_map_inverse, positions)
+    decode_output_display(config, segments_map_inverse, queue, [matching_number | output])
   end
 
-  def validate_input(_, _, [], is_valid), do: is_valid
+  def validate_input(_, _, []), do: true
 
-  def validate_input(config, segments_map, [input_number | queue], is_valid) do
+  def validate_input(config, @segments_map, [input_number | queue]) do
     positions =
       input_number
       |> String.graphemes()
@@ -137,21 +129,25 @@ defmodule DayEight do
 
     matches =
       0..9
-      |> Enum.reduce(0, fn number, match_count ->
-        number_positions_ref = Map.get(segments_map, number)
-        if positions == number_positions_ref, do: match_count + 1, else: match_count
+      |> Enum.reduce_while(0, fn number, match_count ->
+        number_positions_ref = Map.get(@segments_map, number)
+        cond do
+          match_count > 1 -> {:halt, match_count}
+          positions == number_positions_ref -> {:cont, match_count + 1}
+          true -> {:cont, match_count}
+        end
       end)
 
-    if matches === 1, do: validate_input(config, segments_map, queue, true), else: false
+    if matches === 1, do: validate_input(config, @segments_map, queue), else: false
   end
 
-  def get_valid_display_config([], _, _, configs), do: configs
+  def create_display_configurations([], _, _, configurations), do: configurations
 
-  def get_valid_display_config(
+  def create_display_configurations(
         [current_alignment_indices | queue],
         segment_reference,
-        segments_map,
-        configs
+        @segments_map,
+        configurations
       ) do
     segment_alignment =
       current_alignment_indices
@@ -161,11 +157,12 @@ defmodule DayEight do
         Map.put(segment_acc, segment_name, index + 1)
       end)
 
-    configs_updated = [
-      Map.put(segment_alignment, Map.get(segment_reference, 0) |> Enum.at(0), 0) | configs
-    ]
-
-    get_valid_display_config(queue, segment_reference, segments_map, configs_updated)
+    create_display_configurations(
+      queue,
+      segment_reference,
+      @segments_map,
+      [Map.put(segment_alignment, Map.get(segment_reference, 0) |> Enum.at(0), 0) | configurations]
+    )
   end
 
   def run do
